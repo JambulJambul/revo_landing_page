6ab62ddb891753fba601f393659efced10f71d44
JambulJambul <hilman_fahmi21@yahoo.com>
2024-06-11 13:16:30 +0700

    remove clg from ScrollToTop utils

--- src/utils/ScrollToTop/index.jsx
+++ src/utils/ScrollToTop/index.jsx
@@ -5,7 +5,6 @@ export default function ScrollToTop() {
     const { pathname } = useLocation();
 
     useEffect(() => {
-        console.log("test")
         window.scrollTo(0, 0);
     }, [pathname]);
 
